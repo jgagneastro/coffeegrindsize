984926dd4aa5c7f95fc748271485540386652981
jgagneastro <jonathan.gagne.1@gmail.com>
2019-01-23 16:42:46 -0500

    Added app distributionL

---
+++ App/dist/coffeegrindsize.app/Contents/Resources/lib/python3.6/scipy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SCIPY SETUP.PY
+short_version = '0.19.0'
+version = '0.19.0'
+full_version = '0.19.0'
+git_revision = '29dbc361056df93fe659dcb7362fa2c0e647a14b'
+release = True
+
+if not release:
+    version = full_version
